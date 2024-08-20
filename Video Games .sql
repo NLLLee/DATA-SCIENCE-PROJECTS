fe7164b0d6bf37ebbdedc50a7e30474cb9da698c
NLLLee <87617775+NLLLee@users.noreply.github.com>
2024-08-20 16:58:36 -0400

    Add files via upload

---
+++ Video Games .sql
@@ -0,0 +1,105 @@
+-- Purpose
+-- This data was created to try to answer the question "are video games getting worse?"
+-- There is a lot of talk about video games getting worse every year due to companies trying to
+-- maximize profits instead making quality games so I wanted to explore that idea.
+
+
+-- Content
+-- This data file contains over 13,000 games which includes games ranging from 1977 to the middle of 2020.
+-- Most of the data came from directly from the VGChartz database but some has been manually entered in from other sources.
+
+-- In this project, we'll explore the top 400 best-selling video games created between 1977 and 2020.
+-- We'll compare a dataset on game sales with critic and user reviews to determine whether or not video games
+-- have improved as the gaming market has grown.
+
+-- Database contains two tables.
+-- https://www.kaggle.com/datasets/holmjason2/videogamedata
+-- https://www.mordorintelligence.com/industry-reports/global-gaming-market
+
+
+%%sql
+postgresql:///games
+
+-- Select all information for the top ten best-selling games
+-- Order the results from best-selling game down to tenth best-selling
+
+SELECT *
+    FROM game_sales
+    ORDER BY games_sold DESC
+    LIMIT 10;
+
+
+-- Join games_sales and reviews
+-- Count the number of games where both critic_score and user_score are null
+SELECT COUNT(g.game)
+FROM game_sales g
+LEFT JOIN reviews r
+ON g.game = r.game
+WHERE critic_score IS NULL AND user_score IS NULL;
+
+-- Select release year and average critic score for each year, rounded and aliased
+-- Join the game_sales and reviews tables
+-- Group by release year
+-- Order the data from highest to lowest avg_critic_score and limit to 10 results
+SELECT g.year, ROUND(AVG(r.critic_score),2) AS avg_critic_score
+FROM game_sales g
+INNER JOIN reviews r
+ON g.game = r.game
+GROUP BY g.year
+ORDER BY avg_critic_score DESC
+LIMIT 10;
+
+-- Paste your query from the previous task; update it to add a count of games released in each year called num_games
+-- Update the query so that it only returns years that have more than four reviewed games
+
+SELECT g.year, COUNT(g.game) AS num_games, ROUND(AVG(r.critic_score),2) AS avg_critic_score
+FROM game_sales g
+INNER JOIN reviews r
+ON g.game = r.game
+GROUP BY g.year
+HAVING COUNT(g.game) > 4
+ORDER BY avg_critic_score DESC
+LIMIT 10;
+
+-- Select the year and avg_critic_score for those years that dropped off the critics' favorites list
+-- Order the results from highest to lowest avg_critic_score
+SELECT year, avg_critic_score
+FROM top_critic_years
+EXCEPT
+SELECT year, avg_critic_score
+FROM top_critic_years_more_than_four_games
+ORDER BY avg_critic_score DESC;
+
+-- Select year, an average of user_score, and a count of games released in a given year, aliased and rounded
+-- Include only years with more than four reviewed games; group data by year
+-- Order data by avg_user_score, and limit to ten results
+
+SELECT g.year, COUNT(g.game) AS num_games, ROUND(AVG(r.user_score),2) AS avg_user_score
+FROM game_sales g
+INNER JOIN reviews r
+ON g.game = r.game
+GROUP BY g.year
+HAVING COUNT(g.game) > 4
+ORDER BY avg_user_score DESC
+LIMIT 10;
+
+-- Select the year results that appear on both tables
+SELECT year
+FROM top_user_years_more_than_four_games
+INTERSECT
+SELECT year
+FROM top_critic_years_more_than_four_games;
+
+-- Select year and sum of games_sold, aliased as total_games_sold; order results by total_games_sold descending
+-- Filter game_sales based on whether each year is in the list returned in the previous task
+
+SELECT g.year, SUM(g.games_sold) AS total_games_sold
+FROM game_sales g
+WHERE g.year IN (SELECT year
+FROM top_user_years_more_than_four_games
+INTERSECT
+SELECT year
+FROM top_critic_years_more_than_four_games)
+GROUP BY g.year
+ORDER BY total_games_sold DESC;
+
